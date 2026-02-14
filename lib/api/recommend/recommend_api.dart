3a4f745a24d887b1a4e31e5758e7f99e1e32d95f
Zhenghao-Wen <147913447+Zhenghao-Wen@users.noreply.github.com>
2026-02-14 16:24:54 +0800

    更新 recommend_api.dart

--- lib/api/recommend/recommend_api.dart
+++ lib/api/recommend/recommend_api.dart
@@ -1,15 +1,15 @@
 import 'package:bujuan_music_api/api/api.dart';
 import 'package:bujuan_music_api/api/recommend/entity/recommend_resource_entity.dart';
-import 'package:bujuan_music_api/api/recommend/entity/recommend_song_entity.dart';
+import 'package:bujuan_music_api/api/recommend/entity/recommend_new_song_entity.dart'; // 关键修改：导入新实体
 import 'package:bujuan_music_api/bujuan_music_api.dart';
 import 'package:bujuan_music_api/common/music_interceptors.dart';
 
 mixin RecommendApi {
   ///每日推荐歌曲（需要登录）
   ///
-  Future<RecommendSongEntity?> recommendSongs() async {
+  Future<RecommendNewSongEntity?> recommendNewSong() async {
     return await BujuanMusicManager()
-        .post<RecommendSongEntity>(url: Api.recommendSongs, options: createOption());
+        .post<RecommendNewSongEntity>(url: Api.recommendSongs, options: createOption());
   }
 
   ///每日推荐歌单（需要登录）
